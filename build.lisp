f8f1c8ad4657de15758210da85f466beb2070bee
tjohncan <171094307+tjohncan@users.noreply.github.com>
2026-04-09 14:16:19 -0400

    log TLS skip in build: match load order with run-server

--- build.lisp
+++ build.lisp
@@ -1,8 +1,8 @@
 (require :asdf)
 (push *default-pathname-defaults* asdf:*central-registry*)
-(handler-case (asdf:load-system "web-skeleton-tls")
-  (error () nil))
 (asdf:load-system "web-skeleton-demo")
+(handler-case (asdf:load-system "web-skeleton-tls")
+  (error () (format t "Note: TLS not available (libssl not found)~%")))
 (sb-ext:save-lisp-and-die "web-skeleton"
                            :toplevel #'web-skeleton-demo:main
                            :executable t)
