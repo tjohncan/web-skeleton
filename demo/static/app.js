e484279e7a7b3e2519bd1a70df6773d151d28b04
tjohncan <171094307+tjohncan@users.noreply.github.com>
2026-04-09 07:24:32 -0400

    demo: reconnect on send when disconnected

--- demo/static/app.js
+++ demo/static/app.js
@@ -1,6 +1,7 @@
 var log = document.getElementById('log');
 var form = document.getElementById('form');
 var msg = document.getElementById('msg');
+var ws = null;
 
 function appendLog(text, cls) {
   var line = document.createElement('div');
@@ -10,27 +11,38 @@ function appendLog(text, cls) {
   log.scrollTop = log.scrollHeight;
 }
 
-appendLog('[status] connecting...', 'status');
-var ws = new WebSocket('ws://' + location.host + '/ws');
-
-ws.onopen = function() {
-  appendLog('[status] connected', 'status');
-};
-ws.onmessage = function(e) {
-  appendLog('[recv] ' + e.data, 'recv');
-};
-ws.onclose = function() {
-  appendLog('[status] disconnected', 'status');
-};
-ws.onerror = function() {
-  appendLog('[error] connection error', 'err');
-};
+function connect(onOpen) {
+  if (ws && ws.readyState <= 1) return;
+  appendLog('[status] connecting...', 'status');
+  ws = new WebSocket('ws://' + location.host + '/ws');
+  ws.onopen = function() {
+    appendLog('[status] connected', 'status');
+    if (onOpen) onOpen();
+  };
+  ws.onmessage = function(e) {
+    appendLog('[recv] ' + e.data, 'recv');
+  };
+  ws.onclose = function() {
+    appendLog('[status] disconnected', 'status');
+  };
+  ws.onerror = function() {};
+}
 
 form.onsubmit = function(e) {
   e.preventDefault();
   var text = msg.value;
   if (!text) return;
+  if (!ws || ws.readyState !== 1) {
+    connect(function() {
+      ws.send(text);
+      appendLog('[sent] ' + text, 'sent');
+      msg.value = '';
+    });
+    return;
+  }
   ws.send(text);
   appendLog('[sent] ' + text, 'sent');
   msg.value = '';
 };
+
+connect();
