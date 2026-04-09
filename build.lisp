fb45c5289f2af6f25a3e0a98f762085c41d63c2d
tjohncan <171094307+tjohncan@users.noreply.github.com>
2026-04-09 11:42:25 -0400

    try loading TLS in standalone binary build

--- build.lisp
+++ build.lisp
@@ -1,5 +1,7 @@
 (require :asdf)
 (push *default-pathname-defaults* asdf:*central-registry*)
+(handler-case (asdf:load-system "web-skeleton-tls")
+  (error () nil))
 (asdf:load-system "web-skeleton-demo")
 (sb-ext:save-lisp-and-die "web-skeleton"
                            :toplevel #'web-skeleton-demo:main
